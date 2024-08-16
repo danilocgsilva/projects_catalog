cf1c27d1d12e769979edd7bfd921492c5df1a33f
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-16 20:45:12 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="projects_catalog",
+    version="0.1.0",
+    description="Manage the software projects",
+    license="MIT",
+    author="Danilo Silva",
+    author_email="contact@danilocgsilva.me",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.11",
+    ]
+)
