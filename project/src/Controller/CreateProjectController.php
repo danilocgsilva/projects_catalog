a0103c954601b013d1fa01953a8206a770cd7ac5
Danilo Silva <contact@danilocgsilva.me>
2024-12-13 20:09:53 -0300

    Twig added

--- project/src/Controller/CreateProjectController.php
+++ project/src/Controller/CreateProjectController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CreateProjectController extends AbstractController
 {
     #[Route('/create/project', name: 'app_create_project')]
-    public function index(): JsonResponse
+    public function index(): Response 
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CreateProjectController.php',
-        ]);
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/CreateProjectController.php',
+        // ]);
+
+        return $this->render('base.html.twig');
     }
 }
