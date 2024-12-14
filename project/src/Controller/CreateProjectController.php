0bac8c2f9b9001d670017272318f2f3aacc61b5f
Danilo Silva <contact@danilocgsilva.me>
2024-12-14 19:16:48 -0300

    Inherited twig, wip form render

--- project/src/Controller/CreateProjectController.php
+++ project/src/Controller/CreateProjectController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Form\CreateProjectFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CreateProjectController extends AbstractController
 {
     #[Route('/create/project', name: 'app_create_project')]
-    public function index(): Response 
+    public function new(Request $request): Response 
     {
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/CreateProjectController.php',
-        // ]);
+        $project = new Project();
+        
+        $form = $this->createForm(CreateProjectFormType::class);
+        $form->handleRequest($request);
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $data = $form->getData();
+            
+        //     $this->addFlash('success', 'Your message has been sent!');
+        //     return $this->redirectToRoute('contact');
+        // }
 
-        return $this->render('base.html.twig');
+        // return $this->render('base.html.twig');
+        return $this->render('projects/create.html.twig');
     }
 }
