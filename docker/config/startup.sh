1d590222fed749f09a84a453a742b0f4b15b9744
Danilo Silva <contact@danilocgsilva.me>
2024-12-27 19:48:46 -0300

    Updates startup script

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -7,5 +7,9 @@ service apache2 start
 if [ -f $ENV_FILE ]; then
     rm $ENV_FILE
 fi
-cp /var/www/.env.sample /var/www/.env
+cp /var/www/.env.sample $ENV_FILE
+sed -i s/\<USER\>/root/g $ENV_FILE
+sed -i s/\<PASSWORD\>/projectscatalogstrongpassword/g $ENV_FILE
+sed -i s/\<DNS\>/projects_catalog_dev_db/g $ENV_FILE
+sed -i s/\<DATABASENAME\>/projects_catalog/g $ENV_FILE
 while : ; do sleep 1000; done
