f050ccd9890afe17e829f689be656538390350cc
Danilo silva <contact@danilocgsilva.me>
2024-12-27 12:21:46 -0300

    Updated startup script

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+ENV_FILE=/var/www/.env
+
 a2enmod rewrite
 service apache2 start
+if [ -f $ENV_FILE ]; then
+    rm $ENV_FILE
+fi
 cp /var/www/.env.sample /var/www/.env
 while : ; do sleep 1000; done
