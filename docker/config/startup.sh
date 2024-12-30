05a44f4c36dd8cf7a3ad2a0de4805b97c1a7b7c9
Danilo Silva <contact@danilocgsilva.me>
2024-12-29 22:11:57 -0300

    Updated error in startup script

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -12,5 +12,5 @@ sed -i s/\<USER\>/root/g $ENV_FILE
 sed -i s/\<PASSWORD\>/projectscatalogstrongpassword/g $ENV_FILE
 sed -i s/\<DNS\>/projects_catalog_dev_db/g $ENV_FILE
 sed -i s/\<DATABASENAME\>/projects_catalog/g $ENV_FILE
-sed -i s/\<PORT\>/80/g $ENV_FILE
+sed -i s/\<PORT\>/3306/g $ENV_FILE
 while : ; do sleep 1000; done
