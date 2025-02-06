cbf8feb71fa9b20f79a76fb2197a61db9d5010e4
Danilo silva <contact@danilocgsilva.me>
2025-02-06 14:20:34 -0300

    Fixing code receipt
    To workin in Windows

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -10,7 +10,8 @@ fi
 cp /var/www/.env.sample $ENV_FILE
 sed -i s/\<USER\>/root/g $ENV_FILE
 sed -i s/\<PASSWORD\>/projectscatalogstrongpassword/g $ENV_FILE
-sed -i s/\<DNS\>/projects_catalog_dev_db/g $ENV_FILE
+sed -i s/\<DNS\>/$DNS/g $ENV_FILE
 sed -i s/\<DATABASENAME\>/projects_catalog/g $ENV_FILE
 sed -i s/\<PORT\>/3306/g $ENV_FILE
+
 while : ; do sleep 1000; done
