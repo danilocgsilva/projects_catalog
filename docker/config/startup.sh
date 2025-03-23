035f82381503090105e05c9ed556e19c9bc4bd58
Danilo Silva <contact@danilocgsilva.me>
2025-03-23 08:21:27 -0300

    Improvements in the startup script

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -4,12 +4,5 @@ ENV_FILE=/var/www/.env
 
 a2enmod rewrite
 service apache2 start
-if [ -f $ENV_FILE ]; then
-    rm $ENV_FILE
-fi
-
-# symfony server:start --port=8002
 
 symfony server:start --port=8002 --allow-all-ip
-
-# while : ; do sleep 1000; done
