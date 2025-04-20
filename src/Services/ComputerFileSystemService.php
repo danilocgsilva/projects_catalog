50f928d37ab38fb5aa5d76e4f1056c549576c109
Danilo Silva <contact@danilocgsilva.me>
2025-04-20 20:23:44 -0300

    wip: tests for filesystem
    Changes in the implementation for filesystem

--- src/Services/ComputerFileSystemService.php
+++ src/Services/ComputerFileSystemService.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 class ComputerFileSystemService implements DatabaseBackupFileFileSystemInterface
 {
     private const BASE_PATH = __DIR__ . "/../../var/database_backups/";
+
+    public function __construct(private Filesystem $fs)
+    {
+    }
     
     public function exists(string $path): bool
     {
-        return file_exists($this->getFileSystemAddressPath($path));
+        return $this->fs->exists($this->getFileSystemAddressPath($path));
     }
 
     public function getFileSystemAddressPath(string $path): string
