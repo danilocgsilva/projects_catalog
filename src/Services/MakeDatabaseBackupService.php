111f5180e8adc8c5e58a5c29dc2e616dbedab0fe
Danilo Silva <contact@danilocgsilva.me>
2025-05-06 07:55:02 -0300

    Saving database backup to S3

--- src/Services/MakeDatabaseBackupService.php
+++ src/Services/MakeDatabaseBackupService.php
@@ -8,18 +8,29 @@ use App\Repository\DatabaseCredentialRepository;
 use App\Entity\DatabaseBackupFile;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Path;
 use Symfony\Component\HttpKernel\KernelInterface;
+use App\Services\S3FileSystemService;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class MakeDatabaseBackupService
 {
+    private $projectDir;
+
+    private $container;
+
     public function __construct(
         private DatabaseCredentialRepository $databaseCredentialRepository,
         private EntityManagerInterface $entityManager,
-        private KernelInterface $kernel
-    ) {}
+        private KernelInterface $kernel,
+        private ParameterBagInterface $parameterBag,
+        private Filesystem $localFilesystem
+    ) {
+        $this->container = $this->kernel
+            ->getContainer();
+        $this->projectDir = $this->kernel
+            ->getProjectDir();
+    }
     
     public function generateDatabaseBackup(int $databaseId)
     {
@@ -34,19 +45,24 @@ class MakeDatabaseBackupService
             $databaseCredential->getPassword(),
             $databaseCredential->host,
             $databaseCredential->databaseName,
-            $this->kernel->getProjectDir() . "/var/database_backups/" . $fileName,
             (string) $databaseCredential->getPort()
         );
-        $result = shell_exec($shellCommand);
-
+        $backupScriptContent = shell_exec($shellCommand);
+        $this->saveToFileSystem($fileName, $backupScriptContent);
         $this->writeEntryDatabaseBackup($databaseId, $fileName);
+
+        if ($this->parameterBag->get("filesystem_handler") === "s3") {
+            $this->container
+                ->get(S3FileSystemService::class)
+                ->write($fileName, $backupScriptContent);
+        }
     }
 
     private function writeEntryDatabaseBackup(int $databaseId, string $fileName)
     {
-        $databaseBackupFile = new DatabaseBackupFile();
-        $databaseBackupFile->setDate(new DateTime());
-        $databaseBackupFile->setFileName($fileName);
+        $databaseBackupFile = (new DatabaseBackupFile())
+            ->setDate(new DateTime())
+            ->setFileName($fileName);
         $this->entityManager->persist($databaseBackupFile);
         $this->entityManager->flush();
     }
@@ -56,11 +72,10 @@ class MakeDatabaseBackupService
         string $databasePassword,
         string $host,
         string $databaseName,
-        string $fileName,
         string $port = "3306"
     ): string
     {
-        $baseFormat = "mysqldump%s%s%s%s%s > %s";
+        $baseFormat = "mysqldump%s%s%s%s%s";
         
         return sprintf(
             $baseFormat,
@@ -68,8 +83,7 @@ class MakeDatabaseBackupService
             " -p{$databasePassword}",
             " -h{$host}",
             " -P{$port}",
-            " {$databaseName}",
-            "{$fileName}"
+            " {$databaseName}"
         );
     }
 
@@ -78,5 +92,10 @@ class MakeDatabaseBackupService
         $dateTimeNow = (new DateTime())->format("U");
         $databaseCredentialTitle = str_replace(" ", "_", $databaseCredentialName);
         return $databaseCredentialTitle . "-" . $dateTimeNow . ".sql";
+    }
+
+    private function saveToFileSystem(string $fileName, string $content): void
+    {
+        $this->localFilesystem->dumpFile($this->projectDir . "/var/database_backups/" . $fileName, $content);
     }
 }
--- src/Services/S3FileSystemService.php
+++ src/Services/S3FileSystemService.php
@@ -4,28 +4,53 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Symfony\Component\Filesystem\Filesystem;
+use Aws\S3\S3Client;
 
 class S3FileSystemService implements DatabaseBackupFileFileSystemInterface
 {
-    private const BASE_PATH = "s3://my-bucket/database_backups/";
+    private const ADDRESS = "database_backups";
 
-    public function __construct()
-    {
+    private S3Client $s3Client;
+
+    public function __construct(
+        string $s3ClientId,
+        string $s3ClientSecret,
+        private string $s3BucketName
+    ) {
+        $this->s3Client = new S3Client([
+            'region' => 'us-east-1',
+            'version' => '2006-03-01',
+            'credentials' => [
+                'key'    => $s3ClientId,
+                'secret' => $s3ClientSecret
+            ]
+        ]);
     }
     
     public function exists(string $path): bool
     {
-        return $this->fs->exists($this->getFileSystemAddressPath($path));
+        $result = $this->s3Client->doesObjectExist(
+            $this->s3BucketName, 
+            self::ADDRESS . '/' . $path
+        );
+        if ($result) {
+            return true;
+        }
+
+        return false;
     }
 
     public function getFileSystemAddressPath(string $path): string
     {
-        return self::BASE_PATH . $path;
+        return self::ADDRESS;
     }
 
     public function write(string $path, string $content): void
     {
-        
+        $result = $this->s3Client->putObject([
+            'Bucket' => $this->s3BucketName,
+            'Key'    => self::ADDRESS . '/' . $path,
+            'Body'   => $content
+        ]);
     }
 }
