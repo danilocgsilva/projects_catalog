09f3a4044a4699111a4fdc771c6ad257863035a2
Danilo Silva <contact@danilocgsilva.me>
2026-02-08 13:38:24 -0300

    Updated migrations

--- app/migrations/Version20260208163505.php
+++ app/migrations/Version20260208163505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260206084255 extends AbstractMigration
+final class Version20260208163505 extends AbstractMigration
 {
     public function getDescription(): string
     {
